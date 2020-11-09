43153def7816d7d1eb4fbd7a440814d499e86da7
dibran_ho <dibran_ho@outlook.com>
2020-11-09 11:48:04 +0100

    Styled Editor and Preview elements

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -6,10 +6,14 @@ class App extends React.Component {
     return (
       <div>
         <div className="editorWrap">
-          <div class="toolbar"></div>
+          <div class="toolbar">Editor</div>
           <Editor />
         </div>
-        <Preview />
+        <div className="previewWrap">
+          <div class="toolbar">Previewer</div>
+          <Preview />
+        </div>
+
       </div>
     );
   }
@@ -33,5 +37,52 @@ const Preview = props => {
     />
   )
 }
+
+const placeholder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`;
+
+
 
 export default App;
