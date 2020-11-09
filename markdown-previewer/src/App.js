3a38817b468ed5a97aa14b54c836cb1bbce70b44
dibran_ho <dibran_ho@outlook.com>
2020-11-09 11:14:57 +0100

    Added some elments

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+class App extends React.Component {
 
-function App() {
+  render() {
+    return (
+      <div>
+        <div className="editorWrap">
+          <div class="toolbar"></div>
+          <Editor />
+        </div>
+        <Preview />
+      </div>
+    );
+  }
+}
+
+const Editor = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <textarea
+      id='editor'
+      onChange={props.onChange}
+      type='text'
+      value={props.markdown}
+    />
+  )
+}
+
+const Preview = props => {
+  return (
+    <div
+      id="preview"
+    />
+  )
 }
 
 export default App;
